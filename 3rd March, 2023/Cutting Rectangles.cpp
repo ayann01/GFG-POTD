11554b168efa7805af8a5ed663f80df06559479f
Ayan Malhotra <116632852+ayann01@users.noreply.github.com>
2023-03-06 16:43:53 +0530

    Create Cutting Rectangles.cpp

---
+++ 3rd March, 2023/Cutting Rectangles.cpp
@@ -0,0 +1,14 @@
+// User function Template for C++
+
+class Solution{
+public:
+    vector<long long int> minimumSquares(long long int L, long long int B)
+    {
+        // code here
+        long long int x=__gcd(L,B);
+        vector<long long int>ans;
+        ans.push_back((L*B)/(x*x));
+        ans.push_back(x);
+        return ans;
+    }
+};
